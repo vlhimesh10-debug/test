7a8b426c3ed387018fa8ce533aceb0efa365aad6
vlhimesh10-debug <vlhimesh10@gmail.com>
2026-04-17 10:48:17 +0530

    Refactor Test class methods and add division

--- Test.java
+++ Test.java
@@ -1,35 +1,43 @@
 public class Test {
 
   static void add(){
-
-    int a=10;
-    int b=20;
-    int result = a+b;
-    System.out.println("The sum is " +result);
-    
+    int a = 10;
+    int b = 20;
+    int result = a + b;
+    System.out.println("The sum is " + result);
   }
+
   static void sub(){
-
-    int c=20;
-    int d=10;
-    int result1 = c-d;
-    System.out.println("The diff is " +result1);
-    
+    int c = 20;
+    int d = 10;
+    int result1 = c - d;
+    System.out.println("The diff is " + result1);
   }
 
-   static void multi(){
-
-    int x=5;
-    int y=8;
-    int result2 = x*y;
-    System.out.println("The multiplcation result is: " +result2);
-    
+  static void multi(){
+    int x = 5;
+    int y = 8;
+    int result2 = x * y;
+    System.out.println("The multiplication result is: " + result2);
   }
 
-  public static void main(String[] args)
-  {
+  // Division method
+  static void div(){
+    int m = 20;
+    int n = 5;
+
+    if(n != 0){
+      int result3 = m / n;
+      System.out.println("The division result is: " + result3);
+    } else {
+      System.out.println("Cannot divide by zero");
+    }
+  }
+
+  public static void main(String[] args){
     add();
     sub();
     multi();
+    div();   // calling division method
   }
 }
